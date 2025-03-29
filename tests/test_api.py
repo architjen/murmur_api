cb7f4371277db693cf37179e006bd86f76330b7d
architjen <architjen010@gmail.com>
2025-03-29 11:28:05 +0100

    add more test

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,13 +4,20 @@ from app.main import app
 client = TestClient(app)
 
 
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+
+
 def test_ping():
     response = client.get("/hello")
+    print(response)
     assert response.status_code == 200
 
 
 def test_transcribe_endpoint():
     with open("tests/test_audio.wav", "rb") as audio_file:
         response = client.post("/transcribe", files={"file": audio_file})
+    print(response)
     assert response.status_code == 200
     assert "text" in response.json()
