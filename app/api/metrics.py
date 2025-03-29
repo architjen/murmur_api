a50f67455a74f454f592706acdaeb5538ad19914
architjen <architjen010@gmail.com>
2025-03-29 19:06:54 +0100

    adding metrics endpoint

---
+++ app/api/metrics.py
@@ -0,0 +1,36 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from sqlalchemy import text
+
+# custom import(s)
+from app.db.database import get_db
+
+metric_router = APIRouter()
+
+@metric_router.get("/metrics")
+async def get_all_calls(db: Session = Depends(get_db)):
+    query1 = text(f"""
+        SELECT PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY call_latency) AS median_value
+        FROM endpoint_calls;
+    """)
+    result1 = db.execute(query1)
+    median_value_latency = result1.scalar()
+
+    query2 = text(f"""
+        SELECT PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY length) AS median_value
+        FROM endpoint_calls;
+    """)
+    result2 = db.execute(query2)
+    median_value_length = result2.scalar()
+
+    query3 = text(f"""
+        SELECT MAX(id) FROM endpoint_calls;
+    """)
+    result3 = db.execute(query3)
+    total_calls = result3.scalar()
+    
+    return {"median_latency": median_value_latency, 
+            "median_audio_length": median_value_length,
+            "total_no_of_calls": total_calls
+            }
+
--- app/main.py
+++ app/main.py
@@ -2,8 +2,12 @@ from fastapi import FastAPI
 
 # custom import(s)
 from .api.routes import router
+from .api.metrics import metric_router
 
 app = FastAPI(title="Faster Whisperers 🐍")
 
 # Set up routes
 app.include_router(router)
+
+# adding metrics endpoints
+app.include_router(metric_router)
